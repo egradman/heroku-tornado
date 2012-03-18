d35beb07251124c0db39ad3f5c9095669ca8605c
egradman <eric@gradman.com>
2012-03-18 16:50:39 -0700

    mongodb async support

--- server/__main__.py
+++ server/__main__.py
@@ -1,21 +1,54 @@
 import tornado.ioloop
 import tornado.options
 import tornado.web
+from tornado.web import asynchronous
+import tornado.gen as gen
 import logging
+import asyncmongo
 
 import os
+import re
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world")
+# parse the mongo connection URL
+logging.debug(os.getenv('MONGOHQ_URL', "http://localhost"))
+r=re.match(r"mongodb://((?P<dbuser>\w+):(?P<dbpass>\w+)@)?(?P<host>[\w.]+)(:(?P<port>\d+))?/(?P<dbname>\w+)", "mongodb://localhost/test")
+
+mdb_connection_params = dict(
+  pool_id=r.group('dbname'),
+  dbuser= r.group('dbuser'),
+  dbpass= r.group('dbpass'),
+  host=   r.group('host'),
+  port=   r.group('port') and int(r.group('port')) or 27017,
+  dbname= r.group('dbname'),
+  maxcached=10,
+  maxconnections=50
+)
+
+class Handler(tornado.web.RequestHandler):
+    @property
+    def db(self):
+      if not hasattr(self, '_db'):
+        logging.info(str(mdb_connection_params))
+        self._db = asyncmongo.Client(**mdb_connection_params)
+        return self._db
+
+class MainHandler(Handler):
+  @asynchronous
+  @gen.engine
+  def get(self):
+      res = yield gen.Task(self.db.monkeys.find)
+      self.write("Hello, cruel world")
+      self.write("%s" % list(res))
+      self.finish()
 
 application = tornado.web.Application([
     (r"/", MainHandler),
-])
+], debug=True)
 
 if __name__ == "__main__":
     tornado.options.parse_command_line() 
-    port = int(os.getenv('PORT'))
-    logging.info("starting torando web server on port %d", port) 
+
+    port = int(os.getenv('PORT', 8000))
     application.listen(port)
+    logging.info("starting torando web server on port %d", port) 
     tornado.ioloop.IOLoop.instance().start()
